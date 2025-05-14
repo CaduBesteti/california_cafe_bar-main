2d3b77d2557ec4d5a9664b5bcbec128019594131
CaduBesteti <141333538+CaduBesteti@users.noreply.github.com>
2025-05-14 20:35:43 -0300

    voltando ao normal

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -73,6 +73,5 @@ class BarcodesController < ApplicationController
   def cancel_sale
     session[:stocks] = []
     redirect_to barcodes_scan_path, notice: 'Venda cancelada com sucesso!'
-    puts "sexo"
   end
 end
